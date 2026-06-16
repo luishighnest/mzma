338d8af4620ed47db00c5016fc5436b2513d9ffc
luishighnest <147974866+luishighnest@users.noreply.github.com>
2026-06-16 23:50:18 +0200

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Apple TV Media Center</h1>
+    </main>
+  );
+}
